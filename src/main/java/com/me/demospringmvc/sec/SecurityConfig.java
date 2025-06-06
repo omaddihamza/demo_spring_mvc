fe49e037e20961b39ee146ca4ff6065e86bd674b
omaddihamza <hamza.omaddi@gmail.com>
2025-06-06 18:29:35 +0100

    add Security Config

---
+++ src/main/java/com/me/demospringmvc/sec/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.me.demospringmvc.sec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+   public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        PasswordEncoder encoder = passwordEncoder();
+       return new InMemoryUserDetailsManager(
+               User.withUsername("user").password(passwordEncoder().encode("1234")).roles("USER").build(),
+               User.withUsername("admin").password(passwordEncoder().encode("1234")).roles("USER", "ADMIN").build()
+       );
+   }
+
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/user/**").hasRole("USER"))
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/admin/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/public/**").permitAll())
+                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+                .build();
+   }
+
+   @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+   }
+}
